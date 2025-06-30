27792ad3db771a604b79353a6c1227a913c87e87
KilliasBouilletEri <killias.bouillet@erium.fr>
2025-06-30 10:47:49 +0200

    organiZé

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const authRoutes = require('./routes/auth');
+const roomsRouter = require('./routes/rooms');
 const authMiddleware = require('./middlewares/authMiddleware');
 const app = express();
 const { PrismaClient } = require('@prisma/client');
@@ -12,6 +13,7 @@ const { z } = require('zod');
 
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use('/rooms', roomsRouter);
 
 const userSchema = z.object({
   name: z.string().min(1),
@@ -22,8 +24,6 @@ app.get('/', (req, res) => {
   res.send('Serveur OK 👍');
 });
 
-const roomsRouter = require('./routes/rooms');
-app.use('/rooms', roomsRouter);
 
 app.get('/profile', authMiddleware, (req, res) => {
   res.json({ message: `Vous êtes connecté en tant que user ${req.userId}` });
