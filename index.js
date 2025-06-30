eaa6ea33b5e6bb3498c005905d049f12f10156fd
Romain <mast3rsushi@gmail.com>
2025-06-30 11:43:29 +0200

    changement pour utiliser l'auth

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const express = require('express');
 const authRoutes = require('./routes/auth');
 const roomsRouter = require('./routes/rooms');
 const authMiddleware = require('./middlewares/authMiddleware');
-const app = express();
 const { PrismaClient } = require('@prisma/client');
-
+const app = express();
 
 app.use(express.json());
+
+// 🟢 Routes publiques (ex: login, register)
 app.use('/auth', authRoutes);
+
+// 🔐 Middleware d'auth appliqué à tout ce qui suit
+app.use(authMiddleware);
+
+// 🔒 Toutes les routes après ici sont protégées
 app.use('/rooms', roomsRouter);
+
+app.get('/profile', (req, res) => {
+  res.json({ message: `Vous êtes connecté en tant que user ${req.userId}` });
+});
 
 app.get('/', (req, res) => {
   res.send('Serveur OK 👍');
 });
-
-
-app.get('/profile', authMiddleware, (req, res) => {
-  res.json({ message: `Vous êtes connecté en tant que user ${req.userId}` });
-});
-
 
 app.listen(3000, () => {
   console.log('Serveur lancé sur http://localhost:3000');
