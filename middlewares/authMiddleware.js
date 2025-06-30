6807c142f942efb71e6bc2969bc0f238b6074819
KilliasBouilletEri <killias.bouillet@erium.fr>
2025-06-30 12:04:21 +0200

    user dans requete

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
 
-function authMiddleware(req, res, next) {
+async function authMiddleware(req, res, next) {
   const auth = req.headers.authorization;
   if (!auth || !auth.startsWith('Bearer ')) return res.status(401).json({ error: 'No token' });
 
@@ -8,6 +9,15 @@ function authMiddleware(req, res, next) {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
+    
+    // Récupération de l'utilisateur en base
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+    });
+
+    if (!user) return res.status(404).json({ error: 'Utilisateur introuvable' });
+
+    req.user = user;
     next();
   } catch (e) {
     res.status(403).json({ error: 'Invalid token' });
